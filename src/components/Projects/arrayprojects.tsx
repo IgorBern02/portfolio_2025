63969a5859250334fe8af11beb1400316d4147ad
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-31 14:34:45 -0300

    Adicionando imagens de projetos

--- src/components/Projects/arrayprojects.tsx
+++ src/components/Projects/arrayprojects.tsx
@@ -1,7 +1,11 @@
 import Appapifilmes from "../../assets/images/projects/appapifilmes.png";
 import N8nIA from "../../assets/images/projects/n8nIA.png";
-import LoginValidado from "../../assets/images/projects/loginvalidado.png";
+// import LoginValidado from "../../assets/images/projects/loginvalidado.png";
 import SistemaBanco from "../../assets/images/projects/sistemabanco.png";
+import TodoList from "../../assets/images/projects/todolist.png";
+import BelezaEstilo from "../../assets/images/projects/beleza&estilo.png";
+import APIcars from "../../assets/images/projects/apicars.png";
+import GerarCertificado from "../../assets/images/projects/gerarcertificado.png";
 import { Project } from "@/src/types/types";
 
 export const arrayprojects: Project[] = [
@@ -20,17 +24,31 @@ export const arrayprojects: Project[] = [
     site: "https://igorbern02.github.io/n8n-css-ia/",
   },
   {
-    img: LoginValidado,
-    name: "Login Validação",
-    desc: "Login com validação feito com Firebase.",
-    github: "https://github.com/IgorBern02/Login_Validado",
-    site: "https://igorbern02.github.io/Login_Validado/",
+    img: TodoList,
+    name: "Todo List",
+    desc: "Projeto de lista de tarefas simples e funcional. Permite adicionar, filtrar, editar e remover tarefas, com persistência dos dados via API local (json-server).",
+    github: "https://github.com/IgorBern02/ToDoList",
+    site: "https://igorbern02.github.io/ToDoList/",
   },
   {
-    img: SistemaBanco,
-    name: "Sistema Banco",
-    desc: "Um sistema de banco padrão, onde mostra o saldo da conta e podemos simular saques e depósitos.",
-    github: "https://github.com/IgorBern02/Sistema-Banco",
-    site: "https://igorbern02.github.io/Sistema-Banco/",
+    img: BelezaEstilo,
+    name: "Beleza & Estilo",
+    desc: "Prótotipo de site para um salão de beleza, resposivo e feito com react, vite, tailwind e framer-motion. Com simulação de agendamentos via WhatsApp.",
+    github: "https://github.com/IgorBern02/salaodebeleza",
+    site: "https://igorbern02.github.io/salaodebeleza/",
+  },
+  {
+    img: APIcars,
+    name: "API de Carros",
+    desc: "Uma API de carros que permite consultar informações sobre diversos modelos, incluindo marca, modelo, ano e preço. Desenvolvida com Node.js e Express. E o frontend foi feito com React e Tailwind.",
+    github: "https://github.com/IgorBern02/cars_site",
+    site: "https://cars-site-ochre.vercel.app/",
+  },
+  {
+    img: GerarCertificado,
+    name: "Gerador de Certificado",
+    desc: "GeradorCertificado é uma ferramenta rápida e intuitiva para criar certificados personalizados online. Insira os dados desejados, e em poucos cliques gere um certificado visualmente agradável, pronto para download ou impressão.",
+    github: "https://github.com/IgorBern02/gerador-certificado",
+    site: "gerador-certificado-beta.vercel.app",
   },
 ];
